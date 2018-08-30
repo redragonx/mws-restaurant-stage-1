7d3a6d91ac3852631e815453cd13e7f6d226a308
Stephen Chavez <stephen@dicesoft.net>
2018-08-29 18:07:22 -0600

    some work done

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var cors = require('cors')
+var app = express()
+
+
+app.use(cors());
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
+app.use(express.static(__dirname + '/'));
+
+
+
+app.listen('3000');
+console.log('working on 3000');
