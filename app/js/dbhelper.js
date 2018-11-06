cabf6772a9b4daf8d9950b835015a255858652df
Stephen Chavez <stephen@dicesoft.net>
2018-11-06 12:27:26 -0700

    added offline idb

--- app/js/dbhelper.js
+++ app/js/dbhelper.js
@@ -1,6 +1,7 @@
 MWS_DB_NAME = 'mws-db';
-DB_TABLE_NAME = 'mws-restaurants';
+DB_RESTAURANTS_TABLE_NAME = 'mws-restaurants';
 DB_REVIEW_TABLE_NAME = 'reviews';
+DB_PENDING_REVIEW_TABLE_NAME = 'PendingReviews';
 
 /**
  * Common database helper functions.
@@ -12,15 +13,20 @@ class DBHelper {
 
         console.log('opening the IDB for mws');
 
-        let dbPromise = idb.open(MWS_DB_NAME, 1, upgradeDb => {
+        let dbPromise = idb.open(MWS_DB_NAME, 2, upgradeDb => {
             switch (upgradeDb.oldVersion) {
                 case 0:
-                    var mwsDBStore = upgradeDb.createObjectStore(DB_TABLE_NAME, {keyPath: 'id'});
+                    var mwsDBStore = upgradeDb.createObjectStore(DB_RESTAURANTS_TABLE_NAME, {keyPath: 'id'});
                     mwsDBStore.createIndex('by-id', 'id');
 
                 case 1:
                     var reviewsStore = upgradeDb.createObjectStore(DB_REVIEW_TABLE_NAME, {keyPath: 'id'});
                     reviewsStore.createIndex('by-id', 'id');
+
+                case 2:
+                    var tempReviewStore = upgradeDb.createObjectStore(DB_PENDING_REVIEW_TABLE_NAME, {keyPath: 'restaurant_id'});
+                    tempReviewStore.createIndex('by-id', 'id');
+
             }
         });
 
@@ -43,12 +49,28 @@ class DBHelper {
         return dataURL;
     }
 
+    /**
+   * Review URL.
+   */
+    static get REVIEW_URL() {
+        const port = 1337 // Change this to your server port
+        let dataURL = "";
+        if (location.protocol.indexOf("https") !== -1) {
+            dataURL = `https://${location.hostname}:${port}/reviews/`;
+        } else {
+            dataURL = `http://${location.hostname}:${port}/reviews/`;
+        }
+
+        return dataURL;
+    }
+
     static fetchDataFromAPI(callback) {
         fetch(DBHelper.DATABASE_URL).then(function(response) {
             if (response.ok) {
                 //console.log("Response OK", response);
                 return response.json();
             }
+
             console.log("fetch failed response", response);
         }).then(function(json) {
             // console.log("test fetch:", json);
@@ -57,33 +79,16 @@ class DBHelper {
             // Insert the mws data into the idb HERE
 
             restaurants.forEach(function(restaurant) {
-                DBHelper.insertDataIDB(restaurant);
+                DBHelper.insertDataIDB(DB_RESTAURANTS_TABLE_NAME, restaurant);
             });
             callback(null, restaurants);
         }).catch(function(err) {
-            console.log("Some error appended", err);
-
-            DBHelper.openDB().then(function (db) {
-                db.onError = function(evt) {
-                    console.log("error getting something from the idb");
-                    return;
-                };
-
-                let tx = db.transaction(DB_TABLE_NAME, 'readonly');
-                let store = tx.objectStore(DB_TABLE_NAME);
-
-                console.log('idb ', 'getting IDB: ' + data);
-                return store.getAll();
-
-            }).then(restaurants => {
-                console.log(restaurants);
-                callback(null, restaurants);
-            });
+            DBHelper.getAllRestaurantsFromIDB(callback);
         });
     }
 
     /* Method to insert in an IDB */
-    static insertDataIDB(data) {
+    static insertDataIDB(tableName, data) {
 
         return DBHelper.openDB().then(function(db) {
             db.onError = function(evt) {
@@ -91,10 +96,10 @@ class DBHelper {
                 return;
             };
 
-            let tx = db.transaction(DB_TABLE_NAME, 'readwrite');
-            let store = tx.objectStore(DB_TABLE_NAME);
-
-            // console.log('Inserting in IDB: ' + data);
+            let tx = db.transaction(tableName, 'readwrite');
+            let store = tx.objectStore(tableName);
+            //console.log(store);
+            //console.log('Inserting in IDB: ' + data);
             store.put(data);
 
             return tx.complete;
@@ -134,9 +139,236 @@ class DBHelper {
         xhr.send();
     }
 */
+    static addReviewsToIDB(reviews) {
+
+        reviews.forEach(function(review) {
+            // review.id = parseInt(review.id);
+            DBHelper.insertDataIDB(DB_REVIEW_TABLE_NAME, review);
+        });
+        console.log("I am called.");
+
+    }
+
+    /**
+     * Fetch all reviews for a specific restaurant
+     */
+    static fetchRestaurantReviews(restaurant, callback) {
+
+        fetch(DBHelper.REVIEW_URL + "?restaurant_id=" + restaurant.id).then((reviewResp) => {
+            if (!reviewResp.ok) {
+                callback("Failed to get the restaurant review json for id: " + restaurant.id, null);
+            } else {
+                return reviewResp.json();
+            }
+        }).then((reviews) => {
+            if (reviews) {
+                // Creates a full restaurant object before calling a callback.
+                restaurant["reviews"] = reviews;
+                DBHelper.addReviewsToIDB(reviews);
+                callback(null, restaurant);
+                // console.log("review", restaurant);
+            } else {
+                // Has no reviews.
+                callback(null, restaurant);
+            }
+        }).catch(function(error) {
+
+            // Try to load reviews from IDB
+            DBHelper.getReviewsFromIDB(restaurant, callback);
+        });
+
+    }
+
     /**
      * Fetch a restaurant by its ID.
      */
+    static fetchRestaurantById(id, callback) {
+
+        // Get the restaurant from the server
+
+        fetch(DBHelper.DATABASE_URL + id).then((restaurantResp) => {
+            //console.log(restaurantResp);
+            if (!restaurantResp.ok) {
+                callback("Failed to get the restaurant json for id: " + id, null);
+            } else {
+                return restaurantResp.json();
+            }
+        }).then((restaurant) => {
+
+            if (restaurant) {
+                // now get the restaurant reviews before returning the actual restaurant json
+
+                DBHelper.fetchRestaurantReviews(restaurant, callback);
+                //callback(null, restaurant);
+            } else {
+                // Restaurant does not exist in the database
+                callback("Restaurant does not exist", null);
+            }
+        }).catch(function(error) {
+            // Try to load from the idb instead
+            console.log(error);
+            DBHelper.getRestaurantFromIDB(id, callback);
+        });
+    }
+
+    /**
+      * fetch a single restaurant from idb
+      */
+    static getRestaurantFromIDB(id, callback) {
+        // load from idb instead...
+        console.log("Some error appended, but loading from idb", err);
+
+        DBHelper.openDB().then(function(db) {
+            db.onError = function(evt) {
+                console.log("error getting restaurant from the idb: " + id);
+                return;
+            };
+
+            let tx = db.transaction(DB_RESTAURANTS_TABLE_NAME, 'readonly');
+            let store = tx.objectStore(DB_RESTAURANTS_TABLE_NAME);
+
+            // Make a request to get a record by key from the object store
+            let objectStoreRequest = objectStore.get(id);
+            let restaurant = {};
+
+            objectStoreRequest.onsuccess = function(event) {
+                // report the success of our request
+
+                restaurant = objectStoreRequest.result;
+            };
+
+            objectStoreRequest.onerror = function(event) {
+                callback("No Restaurant found", null);
+                return;
+            };
+
+            return restaurant;
+
+        }).then(restaurant => {
+            // console.log(restaurant);
+            DBHelper.fetchRestaurantReviews(restaurant, callback);
+        });
+    }
+
+    /**
+      * fetch a single restaurant from idb
+      */
+    static getReviewsFromIDB(restaurant, callback) {
+        // load from idb instead...
+        console.log("Some error appended, but loading reviews from idb", err);
+
+        DBHelper.openDB().then(function(db) {
+            db.onError = function(evt) {
+                console.log("error getting reviews from the idb");
+                return;
+            };
+
+            let tx = db.transaction(DB_REVIEW_TABLE_NAME, 'readonly');
+            let store = tx.objectStore(DB_REVIEW_TABLE_NAME);
+
+            let reviews = null;
+            store.openCursor().onsuccess = function(event) {
+
+                reviews = [];
+                var cursor = event.target.result;
+                if (cursor && cursor.value.restaurant_id === restaurant.id) {
+                    reviews.push(cursor.value);
+                    cursor.continue();
+                }
+            };
+
+            store.openCursor().onerror = function(event) {
+                // report the success of our request
+
+                callback("No review store", null);
+                return;
+            };
+
+            return reviews;
+        }).then(reviews => {
+            if (reviews) {
+                restaurant["reviews"] = reviews;
+                callback(null, restaurant);
+            } else {
+                callback(null, restaurant);
+            }
+            console.log('got all reviews for restaurant: ' + restaurant.id);
+        });
+    }
+
+    /**
+      * fetch a single restaurant from idb
+      */
+    static getRestaurantFromIDB(id, callback) {
+        // load from idb instead...
+        console.log("Some error appended, but loading from idb", err);
+
+        DBHelper.openDB().then(function(db) {
+            db.onError = function(evt) {
+                console.log("error getting something from the idb");
+                return;
+            };
+
+            let tx = db.transaction(DB_RESTAURANTS_TABLE_NAME, 'readonly');
+            let store = tx.objectStore(DB_RESTAURANTS_TABLE_NAME);
+
+            // Make a request to get a record by key from the object store
+            let objectStoreRequest = objectStore.get(id);
+            let restaurant = null;
+
+            objectStoreRequest.onsuccess = function(event) {
+                // report the success of our request
+
+                restaurant = objectStoreRequest.result;
+            };
+
+            objectStoreRequest.onerror = function(event) {
+                // report the success of our request
+
+                callback("No Restaurant found", null);
+                return;
+            };
+
+            return restaurant;
+
+        }).then(restaurant => {
+            // console.log(restaurant);
+            if (restaurant) {
+                DBHelper.fetchRestaurantReviews(restaurant, callback);
+                callback(null, restaurant);
+
+            } else {
+                callback("No restaurant", null);
+            }
+        });
+    }
+
+    /**
+      * fetch all restaurants from idb
+      */
+    static getAllRestaurantsFromIDB(callback) {
+        // load from idb instead...
+        console.log("Some error appended, but loading from idb", err);
+
+        DBHelper.openDB().then(function(db) {
+            db.onError = function(evt) {
+                console.log("error getting something from the idb");
+                return;
+            };
+
+            let tx = db.transaction(DB_RESTAURANTS_TABLE_NAME, 'readonly');
+            let store = tx.objectStore(DB_RESTAURANTS_TABLE_NAME);
+
+            console.log('idb ', 'getting IDB: ' + data);
+            return store.getAll();
+
+        }).then(restaurants => {
+            console.log(restaurants);
+            callback(null, restaurants);
+        });
+    }
+
+    /*
     static fetchRestaurantById(id, callback) {
         // fetch all restaurants with proper error handling.
         DBHelper.fetchRestaurants((error, restaurants) => {
@@ -154,6 +386,8 @@ class DBHelper {
             }
         });
     }
+    */
+
     /**
    * Fetch restaurants by a cuisine type with proper error handling.
    */
--- app/js/restaurant_info.js
+++ app/js/restaurant_info.js
@@ -199,7 +199,7 @@ createReviewHTML = (review) => {
     li.appendChild(name);
 
     const date = document.createElement('p');
-    date.innerHTML = review.date;
+    date.innerHTML = new Date(review.createdAt).toLocaleDateString();
     li.appendChild(date);
 
     const rating = document.createElement('p');
